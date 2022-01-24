4a361b033fd65c60b7983afcbd6ef1a5935a2fcc
wangxiao <wangxiao@guadoutech.com>
2022-01-24 14:48:12 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-use std::sync::{Mutex, Arc, mpsc::channel};
-use std::thread::spawn;
-
 struct Server {
     ws_sender: ws::Sender,
-    tx: Arc<Mutex<std::sync::mpsc::Sender<String>>>,
 }
 
 impl ws::Handler for Server {
@@ -18,22 +14,10 @@ impl ws::Handler for Server {
 
     fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> {
         self.ws_sender.broadcast(msg.clone());
-        self.tx.lock().unwrap().send(msg.to_string());
         Ok(())
     }
 }
 
 fn main() {
-    let (tx, rx) = channel();
-
-    let x = Arc::new(Mutex::new(tx));
-
-    spawn(move ||{
-        loop {
-            let received = rx.recv().unwrap();
-            println!("Got: {}", received);
-        }
-    });
-
-    ws::listen("0.0.0.0:8085", |ws_sender| Server { ws_sender, tx: x.clone()}).unwrap();
+    ws::listen("0.0.0.0:8085", |ws_sender| Server { ws_sender}).unwrap();
 }
